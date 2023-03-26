960c1977ff7842df3a2df395f7c449319023fbba
Yasen Simeonov <yasen@formenn.is>
2023-03-26 20:29:39 +0300

    Initial commit

---
+++ WindowsServiceCreation/FileWriteService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Pipes;
+
+namespace WindowsServiceCreation
+{
+    public class FileWriteService : ServiceBase
+    {
+
+        public Thread Worker = null;
+
+        public FileWriteService()
+        {
+            ServiceName = "MyCoreService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ThreadStart start = new ThreadStart(Working);
+            Worker = new Thread(start);
+            Worker.Start();
+        }
+
+        public void Working()
+        {
+            int nsleep = 1; // Declare interval as 1 minute.
+
+            try
+            {
+                while (true)
+                {
+                    string FileName = @"E:\TestService\MyCoreService.txt";
+                    using (StreamWriter writer = new StreamWriter(FileName, true))
+                    {
+                        writer.WriteLine(string.Format("Windows Service called on " + DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss tt") + ""));
+                        writer.Close();
+                    }
+                    Thread.Sleep(nsleep * 60 * 1000);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                if (Worker != null & Worker.IsAlive)
+                {
+                    string FileName = @"E:\TestService\MyCoreService.txt";
+                    using (StreamWriter writer = new StreamWriter(FileName, true))
+                    {
+                        writer.WriteLine(string.Format("Windows Service stopped on " + DateTime.Now.ToString("dd.MM.yyyy hh:mm:ss tt") + ""));
+                        writer.Close();
+                    }
+                    Worker.Abort();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+    }
+}
---
+++ WindowsServiceCreation/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ServiceProcess;
+using WindowsServiceCreation;
+
+using (var service = new FileWriteService())
+{
+    ServiceBase.Run(service);
+    //service.OnDebug();
+}
